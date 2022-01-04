b6a61bec37ad736a0f520e8c2d79fa765a5bc7a2
m4n50n <josegarciarodriguez89@hotmail.com>
2022-01-04 13:24:01 +0000

    feat: add form validation by objects

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,33 @@
-const form = document.querySelector("form")
-const form_elements = form.elements; // https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/elements
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    
-    let validation = true;
-    for (let form_item of form_elements) {
-        if (form_item.tagName === "BUTTON") { continue; } // form.elements selects all the elements of the form, but we only want to evaluate the input elements
-
-        form_item.classList.remove("is-valid", "is-invalid"); // delete previous classes
-
-        if (!check_empty_value(form_item.value)) {
-            validation = false;
-            form_item.classList.add("is-invalid");
-        }
-        else {
-            form_item.classList.add("is-valid");
-        }
-    }
-
-    if (validation) {
-        alert("Is a valid form submission!");
-    }
-});
-
-const check_empty_value = (value) => {
-    return (value.trim().length === 0) ? false : true;
+const Form = document.querySelector("form");
+const FormElements = {
+    card: null,
+    cvc: null,
+    amount: null,
+    first_name: null,
+    last_name: null,
+    city: null,
+    state: null,
+    postal_code: null,
+    message: null
 }
+
+const CheckFormElements = () => {
+    FormElements.card = document.querySelector("#card-number");
+    FormElements.cvc = document.querySelector("#cvc");
+    FormElements.amount = document.querySelector("#amount");
+    FormElements.first_name = document.querySelector("#first-name");
+    FormElements.last_name = document.querySelector("#last-name");
+    FormElements.city = document.querySelector("#city");
+    FormElements.state = document.querySelector("#state");
+    FormElements.postal_code = document.querySelector("#postal-code");
+    FormElements.message = document.querySelector("#message");
+
+    for (const property in FormElements) {
+        const FormItem = FormElements[property];
+        FormItem.classList.remove("is-valid", "is-invalid"); // remove previous class
+        FormItem.classList.add((!FormItem.checkValidity()) ? "is-invalid" : "is-valid"); // assign new class according to validity
+    }
+}
+
+Form.addEventListener("change", () => CheckFormElements());
+Form.addEventListener("submit", (e) => (document.querySelectorAll(".is-valid").length === Object.keys(FormElements).length) ? alert("Is a valid form submission!") : e.preventDefault());
